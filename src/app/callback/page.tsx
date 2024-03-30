3597b2fa50fdf5687333d5da495519a19dd7ca8c
Okami <thefoxpaw@protonmail.com>
2024-03-30 12:13:55 -0500

    Add: 404 and updated Readme

---
+++ src/app/callback/page.tsx
@@ -0,0 +1,16 @@
+// Handle call back and send the code to the backend
+
+import Link from 'next/link';
+import LoginButton from '../components/login';
+import { Heading, Text } from '@radix-ui/themes';
+
+
+export default function Home() {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+            URI Callback page 🤔
+
+        </div>
+
+    );
+}
